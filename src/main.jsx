63f04b1e385f227f80e0e8c7d3e19eef0bfb9fb5
yassine <yassineje@icloud.com>
2025-06-13 10:35:52 +0200

    Back to Previous Version for changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,6 @@ import App from './App.jsx'
 import Home from './features/home/Home.jsx'
 import Reader from './features/reader/Reader.jsx'
 import Discover from './features/discover/Discover.jsx'
-import Adventure from './features/adventure/Adventure.jsx'
-import Levels from './features/adventure/Levels/Levels.jsx'
-import Quiz from './features/adventure/Quiz/Quiz.jsx'
 import './styles/index.css'
 
 const router = createBrowserRouter([
@@ -17,10 +14,8 @@ const router = createBrowserRouter([
         children: [
             { index: true, element: <Home /> },
             { path: 'read/:storyId', element: <Reader /> },
-            { path: 'discover', element: <Discover /> },
-            { path: 'adventure', element: <Adventure /> },
-            { path: 'adventure/chapter/:chapterId', element: <Levels /> },
-            { path: 'adventure/level/:levelId', element: <Quiz /> }        ]
+            { path: 'discover', element: <Discover /> }
+        ]
     }
 ])
 
